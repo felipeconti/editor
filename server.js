f25ad46b9ff8cf7075f864ec2949be57ff26a531
Felipe <felipe.conti@totvs.com.br>
2016-05-18 08:22:38 -0300

    Alterado para ws

--- server.js
+++ server.js
@@ -1,74 +1,32 @@
-#!/usr/bin/env node
-var WebSocketServer = require('websocket').server;
-var http = require('http');
-var fs = require('fs');
+var server = require('http').createServer();
+var url = require('url');
+var WebSocketServer = require('ws').Server;
+var wss = new WebSocketServer({ server: server });
+var express = require('express');
+var app = express();
+var port = 8080;
+ 
+app.use(express.static(__dirname + "/public"));
+ 
+wss.on('connection', function connection(ws) {
+    var location = url.parse(ws.upgradeReq.url, true);
+    // you might use location.query.access_token to authenticate or share sessions 
+    // or ws.upgradeReq.headers.cookie (see http://stackoverflow.com/a/16395220/151312) 
+ 
+    ws.on('message', function incoming(message) {
+        console.log('received: %s', message);
 
-const PORT = 8080;
-
-var server = http.createServer(function(request, response) {
-    console.log((new Date()) + ' Received request for ' + request.url);
-
-    fs.readFile('index.html', function(err, data) {
-        response.writeHead(200);
-        response.end(data);
-    });
-
-});
-server.listen(PORT, function() {
-    console.log((new Date()) + ' Server is listening on port 8080');
-});
-
-var connections = [];
-
-wsServer = new WebSocketServer({
-    httpServer: server,
-    // You should not use autoAcceptConnections for production
-    // applications, as it defeats all standard cross-origin protection
-    // facilities built into the protocol and the browser.  You should
-    // *always* verify the connection's origin and decide whether or not
-    // to accept it.
-    autoAcceptConnections: false
-});
-
-function originIsAllowed(origin) {
-  // put logic here to detect whether the specified origin is allowed.
-  return true;
-}
-
-wsServer.on('request', function(request) {
-    if (!originIsAllowed(request.origin)) {
-      // Make sure we only accept requests from an allowed origin
-      request.reject();
-      console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
-      return;
-    }
-
-    var connection = request.accept('echo-protocol', request.origin);
-    connections.push(connection);
-    console.log((new Date()) + ' Connection accepted.');
-    connection.on('message', function(message) {
-        if (message.type === 'utf8') {
-            console.log('Received Message: ' + message.utf8Data);
-            connection.sendUTF(message.utf8Data);
-        }
-        else if (message.type === 'binary') {
-            console.log('Received Binary Message of ' + message.binaryData.length + ' bytes');
-            connection.sendBytes(message.binaryData);
-        }
-
-        // rebroadcast command to all clients
-        connections.forEach(function(destination) {
-            destination.sendUTF(message.utf8Data);
+        wss.clients.forEach(function each(client) {
+            client.send(message);
         });
-
     });
-    connection.on('close', function(reasonCode, description) {
+
+    ws.on('close', function(reasonCode, description) {
         console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected.');
-
-        var index = connections.indexOf(connection);
-        if (index !== -1) {
-            // remove the connection from the pool
-            connections.splice(index, 1);
-        }
     });
+});
+ 
+server.on('request', app);
+server.listen(port, function () {
+    console.log('Listening on ' + server.address().port);
 });
