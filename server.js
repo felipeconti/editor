a7aa863b9d77aced9ffe6f95d5e63c1e1bbf50e1
Felipe <felipe.conti@totvs.com.br>
2016-05-18 12:55:23 -0300

    Cache data on server.

--- server.js
+++ server.js
@@ -5,31 +5,35 @@ var wss = new WebSocketServer({ server: server });
 var express = require('express');
 var app = express();
 var port = 8080;
- 
+
+var data;
+
 app.use(express.static(__dirname + "/public"));
  
 wss.on('connection', function connection(ws) {
-    var location = url.parse(ws.upgradeReq.url, true);
-    // you might use location.query.access_token to authenticate or share sessions 
-    // or ws.upgradeReq.headers.cookie (see http://stackoverflow.com/a/16395220/151312) 
+	var location = url.parse(ws.upgradeReq.url, true);
+	// you might use location.query.access_token to authenticate or share sessions 
+	// or ws.upgradeReq.headers.cookie (see http://stackoverflow.com/a/16395220/151312) 
  
-    if (ws.protocol != "training")
-        ws.close(1002, 'Not training!');
+	if (ws.protocol != "training")
+		ws.close(1002, 'Not training!');
 
-    ws.on('message', function incoming(message) {
-        // console.log('received: %s', message);
+	ws.on('message', function incoming(message) {
+		data = message;
 
-        wss.clients.forEach(function each(client) {
-            client.send(message);
-        });
-    });
+		wss.clients.forEach(function each(client) {
+			client.send(data);
+		});
+	});
 
-    ws.on('close', function(reasonCode, description) {
-        console.log((new Date()) + ' Peer disconnected.');
-    });
+	ws.on('close', function(reasonCode, description) {
+		console.log((new Date()) + ' Peer disconnected.');
+	});
+
+	if (data) ws.send(data);
 });
  
 server.on('request', app);
 server.listen(port, function () {
-    console.log('Listening on ' + server.address().port);
+	console.log('Listening on ' + server.address().port);
 });
