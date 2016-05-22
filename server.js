0fea17c25db98517112fa1f31211b0b8664bca70
Felipe Conti <felipeconti18@gmail.com>
2016-05-22 02:40:21 -0300

    Comment unused variable

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var data;
 app.use(express.static(__dirname + "/public"));
  
 wss.on('connection', function connection(ws) {
-	var location = url.parse(ws.upgradeReq.url, true);
+	// var location = url.parse(ws.upgradeReq.url, true);
 	// you might use location.query.access_token to authenticate or share sessions 
 	// or ws.upgradeReq.headers.cookie (see http://stackoverflow.com/a/16395220/151312) 
  
